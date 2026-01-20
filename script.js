6980358c399d849e2d1eb54e80fb159eb436b40d
St4mpe <hampusekmansundin@gmail.com>
2026-01-20 16:26:11 +0100

    1

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const knapp = document.getElementById('knapp');
-    const topp = document.getElementsByClassName('toppen')
+    const topp = document.querySelector('.toppen');
+    const backgrund = document.querySelector('.information');
 
-    knapp.addEventListener('click', function(){
-        if (knapp.classList.contains("dark"))
-        {
-            topp.replace("toppenLight", "toppenDark");
-            knapp.classList.replace("dark", "light");
-            knapp.innerHTML="Switch to Dark-mode";
-        }
-        else if(theme_switch.classList.contains("light"))
-        {
-            topp.replace("toppenLight", "toppenDark");
+    knapp.addEventListener('click', function () {
+
+        if (knapp.classList.contains("light")) {
+            topp.classList.replace("toppenLight", "toppenDark");
+            backgrund.classList.replace("informationLight", "informationDark");
             knapp.classList.replace("light", "dark");
-            knapp.innerHTML="Switch to Light-mode";
+            knapp.textContent = "Switch to Light-mode";
+        } 
+        else {
+            topp.classList.replace("toppenDark", "toppenLight");
+            backgrund.classList.replace("informationDark", "informationLight");
+            knapp.classList.replace("dark", "light");
+            knapp.textContent = "Switch to Dark-mode";
         }
-    })
-})
+
+    });
+});
